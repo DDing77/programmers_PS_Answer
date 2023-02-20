49367b5d8099fd22aacbcb4abb33855d810b128c
DDing77 <devseo2021@gmail.com>
2023-02-20 21:33:44 +0900

    Lv0_암호 해독

---
+++ 120892/P120892/src/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < cipher.length(); i++) {
+            if ((i + 1) % code == 0) {
+                sb.append(cipher.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+}
