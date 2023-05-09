5fbe27d66ddbdd461e2ad7a2f56b48eaf07d20ca
DDing77 <devseo2021@gmail.com>
2023-05-09 09:19:57 +0900

    Lv0_문자 리스트를 문자열로 변환하기

---
+++ 181941/P181941/src/main/kotlin/Solution.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(arr: Array<String>): String {
+        var answer: String = ""
+        arr.forEach {
+            answer += it
+        }
+        return answer
+    }
+}
