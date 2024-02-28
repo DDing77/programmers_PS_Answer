3cd965de65da2d575fd18928f279b3e967b2d14f
DDing77 <devseo2021@gmail.com>
2024-02-28 16:28:49 +0900

    comment: 3진법 뒤집기 주석 추가

---
+++ src/level1/p12982/Solution.java
@@ -0,0 +1,8 @@
+package level1.p12982;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        return answer;
+    }
+}
--- src/level1/p68935/Solution.java
+++ src/level1/p68935/Solution.java
@@ -1,5 +1,14 @@
 package level1.p68935;
 
+/**
+ * Author : DDing77
+ * Problem Name: 3진법 뒤집기
+ * Level : 1
+ * Language : Java
+ * Category : Math, String
+ * Url : https://school.programmers.co.kr/learn/courses/30/lessons/68935
+ */
+
 public class Solution {
 
     public int solution(int n) {
