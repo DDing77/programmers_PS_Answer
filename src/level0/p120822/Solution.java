d57a2440a64c47aa4f7158e8cbde1d096a5cf0b8
DDing77 <devseo2021@gmail.com>
2024-02-29 07:08:37 +0900

    문자열 뒤집기

---
+++ src/level0/p120822/Solution.java
@@ -0,0 +1,18 @@
+package level0.p120822;
+
+/**
+ * Author : DDing77
+ * Problem Name: 문자열 뒤집기
+ * Level : 0
+ * Language : Java
+ * Category : String
+ * Url : https://school.programmers.co.kr/learn/courses/30/lessons/120822
+ */
+
+class Solution {
+
+    public String solution(String my_string) {
+
+        return new StringBuilder(my_string).reverse().toString();
+    }
+}
