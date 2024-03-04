9a095df2137a1a2232186e29aa9da3a4789e377b
DDing77 <devseo2021@gmail.com>
2024-03-04 18:22:25 +0900

    점프와 순간 이동

---
+++ src/level2/p12980/Solution.java
@@ -0,0 +1,17 @@
+package level2.p12980;
+
+/**
+ * Author : DDing77
+ * Problem Name: 점프와 순간 이동
+ * Level : 2
+ * Language : Java
+ * Category : Bit
+ * Url : https://school.programmers.co.kr/learn/courses/30/lessons/12980
+ */
+
+public class Solution {
+
+    public int solution(int n) {
+        return Integer.bitCount(n);
+    }
+}
