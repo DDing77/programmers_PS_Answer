315f381b68fd08b8134bc1fdc7b52d891edff6a7
DDing77 <devseo2021@gmail.com>
2024-03-19 23:15:53 +0900

    대문자로 바꾸기

---
+++ src/level0/p181877/Solution.java
@@ -0,0 +1,18 @@
+package level0.p181877;
+
+/**
+ * Author : DDing77
+ * Problem Name : 대문자로 바꾸기
+ * Level : 0
+ * Language : Java
+ * Category : String
+ * Url : https://school.programmers.co.kr/learn/courses/30/lessons/181877
+ */
+
+class Solution {
+
+    public String solution(String myString) {
+
+        return myString.toUpperCase();
+    }
+}
