a873d97b7c0a8473c8da05a91c10de2066d31b52
DDing77 <devseo2021@gmail.com>
2024-04-23 02:45:08 +0900

    광고 삽입

--- src/level3/p72414/Solution.java
+++ src/level3/p72414/Solution.java
@@ -1,73 +1,63 @@
 package level3.p72414;
 
-import java.util.Arrays;
+/**
+ * Author : DDing77
+ * Problem Name : 광고 삽입
+ * Level : 3
+ * Language : Java
+ * Category : Prefix-Sum
+ * Url : https://school.programmers.co.kr/learn/courses/30/lessons/72414
+ */
 
 class Solution {
 
-    public int[] prefixSum;
+    public long[] prefixSum;
 
     public int[] convertLogToSecond(String log) {
-
-        String start = log.substring(0, 8);
-        String end = log.substring(9);
-        return new int[]{convertTimeStringToSecond(start), convertTimeStringToSecond(end)};
+        String[] logSplit = log.split("-");
+        return new int[]{convertTimeStringToSecond(logSplit[0]), convertTimeStringToSecond(logSplit[1])};
     }
 
     public int convertTimeStringToSecond(String time) {
-
         String[] dateTime = time.split(":");
         return Integer.parseInt(dateTime[0]) * 3600 + Integer.parseInt(dateTime[1]) * 60 + Integer.parseInt(dateTime[2]);
     }
 
     public String solution(String play_time, String adv_time, String[] logs) {
-
-        prefixSum = new int[360000 + 1];
+        prefixSum = new long[360001];
 
         for (String log : logs) {
             int[] secondInfo = convertLogToSecond(log);
-//            System.out.println("secondInfo[0] = " + secondInfo[0]);
-//            System.out.println("secondInfo[1] = " + secondInfo[1]);
             prefixSum[secondInfo[0]]++;
-            prefixSum[secondInfo[1] + 1]--;
+            prefixSum[secondInfo[1]]--;
         }
-//        0 0 0 0 0;
-//        0 1 0 -1 0;
-//        0 0 1 0 -1;
-//        0 1 2 1 0
-//        int max = 0;
+
         for (int i = 1; i < prefixSum.length; i++) {
             prefixSum[i] = prefixSum[i - 1] + prefixSum[i];
         }
         for (int i = 1; i < prefixSum.length; i++) {
             prefixSum[i] = prefixSum[i - 1] + prefixSum[i];
-//            System.out.println("Math.max(max, prefixSum[i]) = " + Math.max(max, prefixSum[i]));
         }
 
-        Arrays.sort(logs);
-        System.out.println(Arrays.toString(logs));
-        String answer = "";
-
+        int playTime = convertTimeStringToSecond(play_time);
         int advTimeSecond = convertTimeStringToSecond(adv_time);
-        int startTime = convertLogToSecond(logs[0])[0];
-        int max = 0;
-        int minStartTime = Integer.MAX_VALUE;
-//        for (int i = 1; i <= convertLogToSecond(logs[logs.length - 1])[1] - advTimeSecond + 1; i++) {
-        for (int i = 1; i < prefixSum.length - advTimeSecond + 1; i++) {
-            int rangeSum = prefixSum[i + advTimeSecond - 1] - prefixSum[i - 1];
+        long max = prefixSum[advTimeSecond - 1];
+        int minStartTime = 0;
+
+        for (int i = advTimeSecond; i <= playTime; i++) {
+            long rangeSum = prefixSum[i] - prefixSum[i - advTimeSecond];
             if (rangeSum > max) {
                 max = rangeSum;
-                minStartTime = i;
+                minStartTime = i - advTimeSecond + 1;
             }
         }
-//        System.out.println("minStartTime = " + minStartTime);
-//        System.out.println("hour = " + minStartTime / 3600);
-//        System.out.println("minStartTime = " + minStartTime / 60);
-//        System.out.println("second = " + minStartTime % 60);
-        return answer;
-    }
 
-    public static void main(String[] args) {
-        Solution test = new Solution();
-        System.out.println(test.solution("50:00:00", "50:00:00", new String[]{"15:36:51-38:21:49", "10:14:18-15:36:51", "38:21:49-42:51:45"}));
+        int resHour = minStartTime / 3600;
+        int resMin = (minStartTime % 3600) / 60;
+        int resSec = minStartTime % 60;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%02d", resHour)).append(":").append(String.format("%02d", resMin)).append(":").append(String.format("%02d", resSec));
+        return sb.toString();
     }
 }
