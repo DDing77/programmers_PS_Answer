37b10ed02da1b6fd3255ad698114a4100ca8bb42
DDing77 <devseo2021@gmail.com>
2024-05-03 13:53:51 +0900

    공백으로 구분하기 1

---
+++ src/level0/p181869/Solution.java
@@ -0,0 +1,18 @@
+package level0.p181869;
+
+/**
+ * Author : DDing77
+ * Problem Name : 공백으로 구분하기 1
+ * Level : 0
+ * Language : Java
+ * Category : String
+ * Url : https://school.programmers.co.kr/learn/courses/30/lessons/181869
+ */
+
+class Solution {
+
+    public String[] solution(String my_string) {
+        String[] answer = my_string.split(" ");
+        return answer;
+    }
+}
