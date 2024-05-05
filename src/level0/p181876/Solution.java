26bd00b86be818262228946061accea2bd945544
DDing77 <devseo2021@gmail.com>
2024-05-05 09:48:07 +0900

    소문자로 바꾸기

---
+++ src/level0/p181876/Solution.java
@@ -0,0 +1,17 @@
+package level0.p181876;
+
+/**
+ * Author : DDing77
+ * Problem Name : 소문자로 바꾸기
+ * Level : 0
+ * Language : Java
+ * Category : String
+ * Url : https://school.programmers.co.kr/learn/courses/30/lessons/181876
+ */
+
+class Solution {
+
+    public String solution(String myString) {
+        return myString.toLowerCase();
+    }
+}
