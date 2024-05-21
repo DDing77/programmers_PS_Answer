06b697282dee568fb56b481b8285f2ecc709aed5
DDing77 <devseo2021@gmail.com>
2024-05-21 10:18:12 +0900

    문자열의 앞의 n글자

---
+++ src/level0/p181907/Solution.java
@@ -0,0 +1,17 @@
+package level0.p181907;
+
+/**
+ * Author : DDing77
+ * Problem Name : 문자열의 앞의 n글자
+ * Level : 0
+ * Language : Java
+ * Category : String
+ * Url : https://school.programmers.co.kr/learn/courses/30/lessons/181907
+ */
+
+class Solution {
+
+    public String solution(String my_string, int n) {
+        return my_string.substring(0, n);
+    }
+}
