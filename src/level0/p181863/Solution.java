2ceb13df76b0391d100aec9d2454e651a7907caf
DDing77 <devseo2021@gmail.com>
2024-06-12 16:53:57 +0900

    rny_string

---
+++ src/level0/p181863/Solution.java
@@ -0,0 +1,17 @@
+package level0.p181863;
+
+/**
+ * Author : DDing77
+ * Problem Name : rny_string
+ * Level : 0
+ * Language : Java
+ * Category : String
+ * Url : https://school.programmers.co.kr/learn/courses/30/lessons/181863
+ */
+
+class Solution {
+
+    public String solution(String rny_string) {
+        return rny_string.replace("m", "rn");
+    }
+}
